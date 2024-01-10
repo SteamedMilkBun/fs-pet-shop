6e36390c9c2b2fcdaa876ec61d657ef4d6ff6ea4
Lance Tse <steam.bao666@gmail.com>
2024-01-09 16:10:29 -0800

    flow created for read command and no command

--- 1-fs/fs.js
+++ 1-fs/fs.js
@@ -1,1 +1,13 @@
+console.log('running fs.js');
 
+const command = process.argv[2];
+
+if (command === "read"){
+    readPets();
+} else if (process.argv.length < 3){
+    //displays its usage, ideally to the [standard error], when invoking without subcommand    
+    console.log("Usage: node fs.js [read | create | update | destroy]");
+}
+
+
+//The app should [exit the process] with a non-zero exit code to indicate that it failed to complete any work.
