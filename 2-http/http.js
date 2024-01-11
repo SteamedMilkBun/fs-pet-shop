4699566e4df72a4d9d762c8591659ed5eac4f9c3
Lance Tse <steam.bao666@gmail.com>
2024-01-10 19:25:47 -0800

    nonbonus complete

--- 2-http/http.js
+++ 2-http/http.js
@@ -14,7 +14,7 @@ const server = http.createServer(function(req, res) {
 
   if (method === "GET" && url === "/pets") {
     console.log('getting all the pets');
-    fs.readFile("../pets.json", "utf-8", (err, text) => {
+    fs.readFile("../petsCopy.json", "utf-8", (err, text) => {
       if (err) {
         console.error(err);
         res.statusCode = 500;
@@ -24,24 +24,30 @@ const server = http.createServer(function(req, res) {
       //const allPets = JSON.parse(fileData);
       res.setHeader('Content-Type', 'application/json');
       res.end(text);
-    })
+    });
 
   } else if (method === "GET" && url.match(petRegExp)) {
       const index = Number(url.match(petRegExp)[1]);
 
-      fs.readFile("../pets.json", "utf-8", (err, text) => {
+      fs.readFile("../petsCopy.json", "utf-8", (err, text) => {
         if (err) {
           console.error(err);
           res.statusCode = 500;
           res.end();
           return;
         }
-      });
-      console.log(`getting pet at index: ${index}`);
+        const pets = JSON.parse(text);
+        console.log(`getting pet at index: ${index}`);
 
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('Get a single pet');
-    
+        if (!Number.parseInt(index) || index < 0 || index >= pets.length) {
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/plain');
+          res.end('Not found');
+          return;
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(pets[index]));
+      });
   }
   
   else {
